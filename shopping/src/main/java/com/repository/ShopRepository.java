3fcbcf91e0027852fca0ddcd8384bfb5d033ec5f
yanghub <67542831+yanghub@users.noreply.github.com>
2020-08-26 16:50:51 +0800

    Add files via upload

---
+++ shopping/src/main/java/com/repository/ShopRepository.java
@@ -0,0 +1,8 @@
+package com.dropshoping.dropshoping.repository;
+
+import com.dropshoping.dropshoping.entity.Shop;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ShopRepository extends JpaRepository<Shop, Integer>{
+
+}
